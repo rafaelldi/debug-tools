b05e8bd6b79501201aec63b852690af03e45e1e0
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2025-04-29 21:39:31 +0200

    Implement a simple http client

--- MonitorClient/Program.cs
+++ MonitorClient/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+using Grpc.Core;
+using Grpc.Net.Client;
+using MonitorClient;
 
-Console.WriteLine("Hello, World!");
+using var channel = GrpcChannel.ForAddress("http://localhost:5197");
+
+var client = new ProcessService.ProcessServiceClient(channel);
+
+var processes = await client.GetProcessListAsync(new ProcessListRequest());
+Console.WriteLine("Available processes:");
+foreach (var process in processes.Processes)
+{
+    Console.WriteLine($"{process.ProcessId} - {process.ProcessName}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Enter process id to get counters:");
+var processId = Console.ReadLine();
+
+using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    // ReSharper disable once AccessToDisposedClosure
+    cts.Cancel();
+};
+
+var counterClient = new CounterService.CounterServiceClient(channel);
+Debug.Assert(processId != null, nameof(processId) + " != null");
+using var counterCall = counterClient.GetCounterStream(new CounterStreamRequest { ProcessId = int.Parse(processId) });
+
+try
+{
+    await foreach (var counter in counterCall.ResponseStream.ReadAllAsync(cts.Token))
+    {
+        Console.WriteLine(
+            $"{counter.Timestamp} - {counter.Name} - {counter.DisplayName} - {counter.ProviderName} - {counter.Value}");
+    }
+}
+catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+{
+    Console.WriteLine("Operation canceled");
+}
