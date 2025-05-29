bcb08d643da6bcebae0a8537ee3e1c4620cdd717
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-05-29 21:23:54 +0200

    Update an agent

--- monitor-agent/src/main.kt
+++ monitor-agent/src/main.kt
@@ -1,9 +1,17 @@
 import ai.koog.agents.ext.agent.simpleSingleRunAgent
+import ai.koog.agents.mcp.McpToolRegistryProvider
+import ai.koog.prompt.executor.llms.all.simpleOllamaAIExecutor
+import ai.koog.prompt.llm.OllamaModels
 
 suspend fun main() {
-    println("Hello, World!")
+    val toolRegistry = McpToolRegistryProvider.fromTransport(
+        transport = McpToolRegistryProvider.defaultSseTransport("http://localhost:5197")
+    )
 
     val agent = simpleSingleRunAgent(
-
+        simpleOllamaAIExecutor("http://localhost:11434"),
+        " You are an assistant helping diagnose different problems with .NET applications.",
+        OllamaModels.Meta.LLAMA_3_2,
+        toolRegistry = toolRegistry
     )
 }
